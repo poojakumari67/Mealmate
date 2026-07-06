b53931b4e3abcfd69bc754434321ef764237879a
Pooja Kumari <260105356+poojakumari67@users.noreply.github.com>
2026-07-06 12:54:15 +0530

    Enable debug

--- Mealmate/Mealmate/settings.py
+++ Mealmate/Mealmate/settings.py
@@ -26,7 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", "your-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "True") == "True"
+#DEBUG = os.environ.get("DEBUG", "True") == "True"
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
